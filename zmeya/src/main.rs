f4dca90178d71bc1bebb1d0fe1dc3a5ed3183a48
SaberOte <andrey.gurt@yandex.ru>
2025-04-11 23:08:06 +0200

    show field

--- zmeya/src/main.rs
+++ zmeya/src/main.rs
@@ -1,5 +1,24 @@
+const FIELD_WIDTH: usize = 30;
+const FIELD_HEIGHT: usize = 30;
+
+
+fn show_field(field: [[char; FIELD_WIDTH]; FIELD_HEIGHT]){
+    let mut field_str = String::from("");
+    for row in field.iter() {
+        for cell in row.iter(){
+            field_str.push(*cell);
+        }
+        field_str.push('\n');
+    }
+    print!("{}", field_str);
+}
+
+fn build_field() {
+    let field: [[char; FIELD_WIDTH]; FIELD_HEIGHT] = [['f'; FIELD_WIDTH]; FIELD_HEIGHT];
+    show_field(field);
+}
 
 
 fn main() {
-    println!("hello zmeya");
+    build_field();
 }
