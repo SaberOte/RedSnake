7b12b85b954252af8afaec909f40da0ff0911018
SaberOte <andrey.gurt@yandex.ru>
2025-04-13 13:02:40 +0200

    removed clear console function

--- zmeya/src/console.rs
+++ zmeya/src/console.rs
@@ -1,40 +1,7 @@
-use windows::{
-    core::*,
-    Win32::System::Console::*,
+use windows::Win32::System::Console::{
+    FreeConsole, AllocConsole
 };
-use windows::Win32::Foundation::HANDLE;
-use std::thread::sleep;
-
-#[cfg(windows)]
-pub fn clear_console_windows() {
-
-    unsafe {
-        let handle = GetStdHandle(STD_OUTPUT_HANDLE).unwrap();
-
-        // Get console info to find the size of the screen buffer
-        let mut console_info = CONSOLE_SCREEN_BUFFER_INFO::default();
-        GetConsoleScreenBufferInfo(handle, &mut console_info).unwrap();
-
-        let console_size = console_info.dwSize.X as u32 * console_info.dwSize.Y as u32;
-        let coord = COORD { X: 0, Y: 0 };
-
-        // Fill the console with spaces
-        let mut chars_written = 0;
-        FillConsoleOutputCharacterA(handle, b' ' as i8, console_size, coord, &mut chars_written).unwrap();
-
-        // Reset the attributes
-        FillConsoleOutputAttribute(
-            handle,
-            console_info.wAttributes.0,
-            console_size,
-            coord,
-            &mut chars_written
-        ).unwrap();
-
-        // Move cursor to top-left corner
-        SetConsoleCursorPosition(handle, coord).unwrap();
-    }
-}
+use windows::core::Result;
 
 
 pub fn create_new_console() -> Result<()> {
